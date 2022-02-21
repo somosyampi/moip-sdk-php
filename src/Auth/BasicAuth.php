1fdefbd918cdd3e73bec26f3e24270338226c12f
Alerson <alerson@ybubb.com.br>
2022-02-21 12:12:52 -0300

    fix method register on BaiscAuth class

--- src/Auth/BasicAuth.php
+++ src/Auth/BasicAuth.php
@@ -41,7 +41,7 @@ class BasicAuth implements Authentication
      *
      * @param \Requests_Hooks $hooks Hook system
      */
-    public function register(Requests_Hooks &$hooks)
+    public function register(Requests_Hooks $hooks)
     {
         $hooks->register('requests.before_request', [&$this, 'before_request']);
     }
